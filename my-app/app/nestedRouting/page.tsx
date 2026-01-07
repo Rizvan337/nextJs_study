33520b7a0aef430903a405464b6e2c4f46a5a257
Rizvan337 <rizvanichu337@gmail.com>
2026-01-07 13:04:48 +0400

    feat: add nested routing using Next.js App Router

--- my-app/app/nestedRouting/page.tsx
+++ my-app/app/nestedRouting/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
 export default function Blog() {
-  return <h1>Blog Page</h1>;
+  return (
+    <>
+      <h1>Nested Routing Page</h1>
+      <ul>
+        <li>
+          <Link href={"/nestedRouting/first"}>First</Link>
+        </li>
+        <li>
+          <Link href={"/nestedRouting/second"}>Second</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
